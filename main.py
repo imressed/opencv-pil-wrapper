f80a12c055ab9be5844f0c18f8745f244bfeca3c
imressed <imressed@gmail.com>
2015-04-23 16:36:44 +0300

    add convert add resize add copy

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ class pilLike(object):
     def __init__(self):
         object.__init__(self)
 
+    @property
+    def size(self):
+        return self._instance.size
+
+    @property
+    def shape(self):
+        return self._instance.shape
+
     def new(self, size, channels, depth):
         self._instance = np.zeros(size + (channels,), depth)
         return self._instance
@@ -20,21 +28,41 @@ class pilLike(object):
     def open(self, fl):
         if isinstance(fl, basestring):
             self._instance = cv2.imread(fl,cv2.IMREAD_UNCHANGED)
+            return self._instance
         if isinstance(fl, file):
             file_bytes = np.asarray(bytearray(fl.read()), dtype=np.uint8)
             self._instance = cv2.imdecode(file_bytes, cv2.CV_LOAD_IMAGE_UNCHANGED)
+            return self._instance
         if hasattr(fl, 'mode'):
             image = np.array(fl)
             self._mode = fl.mode
             if self._mode == 'RGB':
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             self._instance = image
-        return self._instance
+            return self._instance
 
     def crop(self, x1,y1,x2,y2):
         self._instance = self._instance[y1:y2, x1:x2]
         return self._instance
 
+    def copy(self):
+        return self._instance.copy()
+
+    def close(self):
+        cv2.destroyAllWindows()
+        return None
+
+    def resize(self, size, interpolation = cv2.INTER_LINEAR):
+        self._instance = cv2.resize(self._instance, size, interpolation = interpolation)
+        return self._instance
+
+    def convert(self, flag):
+        self._instance = cv2.cvtColor(self._instance, flag)
+        return self._instance
+
+    #TODO: write this
+    def save(self, format, file = _instance):
+        cv2.imwrite(format, file)
 
 
     def show(self):
@@ -45,16 +73,19 @@ class pilLike(object):
 
 
 a = pilLike()
-with open('1.jpg', 'rb') as img:
-    a.open(img)
-
 
 pil_image = Image.open('1.jpg')
-a.open(pil_image)
 
-a.crop(0,300,300,200)
+with open('1.jpg', 'rb') as img:
+    b = a.open(img)
+    a.save('test.jpg', img)
+
+
+a.open(pil_image)
+a.convert(cv2.COLOR_RGB2GRAY)
 
 a.show()
+
 
 
 
