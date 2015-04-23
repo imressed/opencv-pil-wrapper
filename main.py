1c278453895027144e59453a6f4fd93e1b3add6c
imressed <imressed@gmail.com>
2015-04-23 15:21:43 +0300

    add new add open

--- main.py
+++ main.py
@@ -1,2 +1,57 @@
 # -*- coding: utf-8 -*-
 __author__ = 'imressed'
+
+import cv2
+import numpy as np
+from PIL import Image
+from cStringIO import StringIO
+
+
+class pilLike(object):
+    _instance = None
+
+    def __init__(self):
+        object.__init__(self)
+
+    def new(self, size, channels, depth):
+        self._instance = np.zeros(size + (channels,), depth)
+        return self._instance
+
+    def open(self, fl):
+        if isinstance(fl, basestring):
+            self._instance = cv2.imread(fl,cv2.IMREAD_UNCHANGED)
+        if isinstance(fl, file):
+            file_bytes = np.asarray(bytearray(fl.read()), dtype=np.uint8)
+            self._instance = cv2.imdecode(file_bytes, cv2.CV_LOAD_IMAGE_UNCHANGED)
+        if hasattr(fl, 'mode'):
+            image = np.array(fl)
+            self._mode = fl.mode
+            if self._mode == 'RGB':
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            self._instance = image
+
+        return self._instance
+
+
+
+    def show(self):
+        cv2.imshow('ImageWindow',self._instance)
+        cv2.waitKey()
+
+
+
+
+a = pilLike()
+with open('1.jpg', 'rb') as img:
+    a.open(img)
+
+
+pil_image = Image.open('1.jpg')
+a.open(pil_image)
+
+a.show()
+
+
+
+
+
