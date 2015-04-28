17e2f56f28e984826c85b29f1bc54f5c3786c955
imressed <imressed@gmail.com>
2015-04-28 13:52:18 +0300

    fix resize fix convert
    fix draw rectangle(with fill and without)

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from cStringIO import StringIO
 
 class Image(object):
     _instance = None
+    _mode = None
+
+    NEAREST = cv2.INTER_NEAREST
+    BILINEAR = cv2.INTER_LINEAR
+    BICUBIC = cv2.INTER_CUBIC
+    LANCZOS = cv2.INTER_LANCZOS4
 
     def __init__(self):
         object.__init__(self)
@@ -16,6 +22,13 @@ class Image(object):
     @property
     def size(self):
         return self._instance.size
+
+    @property
+    def mode(self):
+        if self._mode:
+            return self._mode
+        else:
+            raise ValueError('No mode specified.')
 
     @property
     def shape(self):
@@ -52,17 +65,55 @@ class Image(object):
 
         raise ValueError('Your mode name is incorect.')
 
+    def _get_converting_flag(self, mode):
+        mode = mode.upper()
+        inst = self._mode.upper()
+        converting_table = {
+            'L':{
+                'RGB':cv2.COLOR_GRAY2BGR
+            },
+            'RGB':{
+                'L':cv2.COLOR_BGR2GRAY,
+                'LAB':cv2.COLOR_BGR2LAB,
+                'HSV':cv2.COLOR_BGR2HSV,
+                'YCBCR':cv2.COLOR_BGR2YCR_CB
+            },
+            'LAB':{
+                'RGB':cv2.COLOR_LAB2BGR
+            },
+            'HSV':{
+                'RGB':cv2.COLOR_HSV2BGR
+            },
+            'YCBCR':{
+                'RGB':cv2.COLOR_YCR_CB2BGR
+            }
+        }
+        if converting_table.has_key(inst):
+            if converting_table[inst].has_key(mode):
+                return converting_table[inst][mode]
+            else:
+                raise ValueError('You can not convert image to this type')
+        else:
+            raise ValueError('This image type can not be converted')
+
+
     def new(self, mode, size, color=(0,0,0)):
+        self._mode = mode
         channels, depth = self._get_channels_and_depth(mode)
         self._instance = np.zeros(size + (channels,), depth)
         self._instance[:,0:] = color
         return self._instance
 
     def open(self, fl, mode='r'):
+        self._mode = None
         if isinstance(fl, basestring):
             self._instance = cv2.imread(fl,cv2.IMREAD_UNCHANGED)
+            pil_instance = PILImage.open(fl)
+            self._mode = pil_instance.mode
             return self._instance
         if isinstance(fl, file):
+            pil_instance = PILImage.open(fl)
+            self._mode = pil_instance.mode
             file_bytes = np.asarray(bytearray(fl.read()), dtype=np.uint8)
             self._instance = cv2.imdecode(file_bytes, cv2.CV_LOAD_IMAGE_UNCHANGED)
             return self._instance
@@ -90,7 +141,10 @@ class Image(object):
         self._instance = cv2.resize(self._instance, size, interpolation = interpolation)
         return self._instance
 
-    def convert(self, flag):
+    def convert(self, mode):
+        if self._mode.upper() == mode.upper():
+            return self._instance
+        flag = self._get_converting_flag(mode)
         self._instance = cv2.cvtColor(self._instance, flag)
         return self._instance
 
@@ -120,28 +174,35 @@ class ImageDraw(object):
     def line(self, xy, color, width):
         cv2.line(self._img_instance,xy[0], xy[1], color, width)
 
-    def rectangle(self, xy, fill, outline):
-        cv2.rectangle(self._img_instance,xy[0], xy[1], fill, outline)
+    def rectangle(self, xy, fill=None, outline=None):
+        if fill:
+            cv2.rectangle(self._img_instance,xy[0], xy[1], fill, thickness=cv2.cv.CV_FILLED)
+        if outline:
+            cv2.rectangle(self._img_instance,xy[0], xy[1], outline, 3)
 
 
 
 
 a = Image()
 
-#a.new('rgb',(300,300),(0,255,0))
-
+a.new('rgb',(300,300),(0,255,0))
+print(a.mode)
+#a.crop((100,100,400,500))
+#a.resize((200,200),Image.BILINEAR)
+#a.convert('hsv')
 
 pil_image = PILImage.open('1.jpg')
 
 with open('1.jpg', 'rb') as img:
     b = a.open(img)
 
-
-
 a.open(pil_image)
+
+
+
 d = ImageDraw(a.get_instance)
 b = a.copy()
-d.rectangle([(10,10),(100,200)], (255,255,0), 5)
+d.rectangle([(10,10),(100,200)], fill=(255,255,0))
 
 
 
